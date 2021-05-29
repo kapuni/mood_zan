da297174b851b5c86416d3e258ff66415750b7e5
910048757@qq.com <90048757@qq.com>
2021-05-29 22:49:32 +0800

    RedisTest

---
+++ mood_a/src/test/java/com/example/mood/RedisTest.java
@@ -0,0 +1,24 @@
+package com.example.mood;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RedisTest {
+
+    @Resource
+    private RedisTemplate<String, Object> redisTemplate;
+
+    @Test
+    public void set(){
+        redisTemplate.opsForValue().set("myKey","myValue");
+        System.out.println(redisTemplate.opsForValue().get("myKey"));
+    }
+}
